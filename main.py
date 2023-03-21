643e3635a70ec465cc11b4c2611b0f40afad4311
Zongyuan Li <lizongy1@msu.edu>
2023-03-20 23:36:04 -0400

    model but is not working at this time

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from data.YTCelebrityDataset import YTCelebrityDataset
 from models.model import Model, train, test
-import cv2
+from torchvision import transforms
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 learning_rate = 0.001
@@ -14,15 +15,32 @@ csv_path = "celebrity.csv"
 def main():
     dataset = YTCelebrityDataset(root, csv_path)
 
+    new_dataset = []
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((128, 128)),
+        transforms.ToTensor()
+    ])
+
+    print("\nconverting...\n")
+    for _, (frames, label) in enumerate(dataset):
+        starting_frame = transform(np.asarray(frames[0]))
+        starting_label = [float(l) for l in label]
+        new_dataset.append((starting_frame, torch.from_numpy(np.asarray(starting_label[:4]))))
+    
+    print("\nsplitting...\n")
     generator = torch.Generator().manual_seed(42)
     training, testing = torch.utils.data.random_split(dataset, [0.7, 0.3], generator=generator)
 
-    train_loader = DataLoader(training, batch_size=4, shuffle=True, num_workers=4)
-    test_loader = DataLoader(testing, batch_size=4, shuffle=True, num_workers=4)
+    train_loader = DataLoader(training, batch_size=1, shuffle=True, num_workers=1)
+    test_loader = DataLoader(testing, batch_size=1, shuffle=True, num_workers=1)
 
     model = Model().to(device)
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     
-    train(model, device, train_loader, optimizer, epoch)
-
+    print("\ntraining...\n")
+    train(model, device, train_loader, optimizer)
+    print("\ntesting...\n")
     test(model, device, test_loader)
+
+main()
--- models/model.py
+++ models/model.py
@@ -5,28 +5,54 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 53 * 53, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(3, 12, 3 )
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(12, 24, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(24 * 31 * 31, 100)
+        self.fc2 = nn.Linear(100, 30)
+        self.fc3 = nn.Linear(30, 4)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 53 * 53)
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(-1, 24 * 31 * 31)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
-def train(model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer):
     model.train()
+    for i, (frame, label) in enumerate(train_loader):
+        print(type(frame))
+        print(type(label))
+        # frame, label = frame.to(device), label.to(device)
+        optimizer.zero_grad()
+        prediction = model(frame)
+        loss = custom_loss(prediction, label)
+        loss.backward()
+        optimizer.step()
+        if i % 100:
+            print(f"Loss: {loss}")
+    
           
 def test(model, device, test_loader):
     model.eval()
+    with torch.no_grad():
+        for i, (frames, label) in enumerate(test_loader):
+            starting_frame = frames[0]
+            starting_frame, label = starting_frame.to(device), label.to(device)
+            prediction = model(starting_frame)
+            loss = custom_loss(prediction, label)
+            if i % 100:
+                print(f"Loss: {loss}")
 
+
+def custom_loss(prediction, label):
+    p_left, p_top, p_scale, p_rot = prediction
+    l_left, l_top, l_scale, l_rot = label
+    return abs(p_left - l_left) + abs(p_top - l_top) + abs(p_scale - l_scale) + abs(p_rot - l_rot)
 
     
 
